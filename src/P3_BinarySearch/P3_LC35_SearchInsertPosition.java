b9bf3e1b67fd8d2ec41cb16655d85a09800c4f1a
Amul Op <amulsharmarr67@gmail.com>
2025-01-18 00:16:09 +0530

    Binary Search Problem LC 35 - Search Insert Position

--- src/P3_BinarySearch/P3_LC35_SearchInsertPosition.java
+++ src/P3_BinarySearch/P3_LC35_SearchInsertPosition.java
@@ -30,4 +30,31 @@ public class P3_LC35_SearchInsertPosition {
         return insertPosition;
 
     }
+
+
+    //Optimised Approach
+    public static int searchInsert2(int[] nums, int target) {
+        int n = nums.length;
+        int startIndex = 0;
+        int endIndex = n-1;
+
+        //In binary search always know if target would be found - thats great
+        //if not found then both index pointer crosses the index
+        // endIndex (pointing to element < target) & startIndex (pointing to element > target)
+        // e.g.  target - 4     [3,5]   [endIndexPointer, startIndexPointer]  means 4 should be coming in place of 5
+        //So, startIndex will have my answer that is the element expected position of target
+        int searchInsertPosition = -1;
+        while(startIndex <= endIndex){
+            int midIndex = startIndex + (endIndex - startIndex)/2;
+            if(nums[midIndex] == target){
+                searchInsertPosition = midIndex;
+                break;
+            }else if(target > nums[midIndex]){
+                startIndex = midIndex + 1;
+            }else{
+                endIndex = midIndex - 1;
+            }
+        }
+        return searchInsertPosition != -1 ? searchInsertPosition : startIndex;
+    }
 }
